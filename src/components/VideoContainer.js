857906c7499f8890f0a747707421a133e293797b
Harshit Tyagi <harshitt152@gmail.com>
2024-01-04 20:33:00 +0530

    Added Video Slice

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -2,22 +2,24 @@ import { useState, useEffect } from "react";
 import VideoCard from "./VideoCard";
 import { YOUTUBE_VIDEOS_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
-// import { useEffect } from "react";
+import { addVideoList } from "../utils/videoSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const VideoContainer = () => {
-  const [videos, setVideos] = useState([]);
+  const videoList = useSelector((store) => store.video.videoList);
+  const dispatch = useDispatch();
   useEffect(() => {
     getVideos();
   }, []);
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_VIDEOS_URL);
     const json = await data.json();
-
-    setVideos(json.items);
+    dispatch(addVideoList(json.items));
   };
   return (
     // <div className="flex flex-wrap">
     <div className="grid grid-cols-4 gap-5">
-      {videos.map((vid) => {
+      {videoList.map((vid) => {
         return (
           <Link key={vid?.id} to={"/watch?v=" + vid?.id}>
             <VideoCard vid={vid} />
--- src/utils/store.js
+++ src/utils/store.js
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
 import searchCacheSlice from "./searchCacheSlice";
 import liveChatSlice from "./LiveChatSlice";
-
+import videoSlice from "./videoSlice";
 const store = configureStore({
   reducer: {
     app: appSlice,
     search: searchCacheSlice,
     chat: liveChatSlice,
+    video: videoSlice,
   },
 });
 export default store;
